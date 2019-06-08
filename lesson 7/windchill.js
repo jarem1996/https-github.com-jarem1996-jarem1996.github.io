08e65d4af4909db17bf55aa01afd823806481979
Jarem1996 <burningblazeofglory@gmail.com>
2019-06-08 13:42:28 -0700

    C-c-c-changes

---
+++ lesson 7/windchill.js
@@ -0,0 +1,8 @@
+// Input - get input and convert that string to an integer
+let x = parseInt(document.getElementById('somevalue').innerHTML);
+
+// Processing - some random formula processing with the variable
+let result = f= 35.74 + 0.6215 t - 35.75 + 0..4275;
+
+// Output - round to one decimal and output string with HTML to innerHTML of a div
+document.getElementById('output').innerHTML = "The result is <strong>" + result.toFixed(1) + "</strong>";
